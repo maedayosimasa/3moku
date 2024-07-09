9d210d7cc34554183f56f3726e78f1652453848f
maedayosimasa <gdre131@yahoo.co.jp>
2024-07-09 13:51:58 +0900

    〇×交互入力

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 
 const cells = document.querySelectorAll(".button");
+console.log(cells);
 let current = "X";
-
+let count = 0;
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener("click", () => {
+        //クリックした回数を数える
+        count++;
+        //奇数は×偶数は〇を入力
+        if (count % 2 === 1) {
+            current = '×'
+        } else {
+            current = '〇'
+        }
         if (cells[i].textContent === "") {
             cells[i].textContent = current;
-            current = current === "X" ? "O" : "X";
+            //三項定理による切り替え
+            // current = current === "X" ? "O" : "X"
         }
     });
 }
