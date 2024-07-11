1088702a9b2dc7248e0acedd07096fb3079f841e
maedayosimasa <gdre131@yahoo.co.jp>
2024-07-11 12:50:18 +0900

    判定追記

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let current = "X";
 let count = 0;
 //判定するための関数
 const judge = (a,b,c) => {
-    return cells[0].textContent  === current && cells[1].textContent  === current && cells[2].textContent  === current }
+    return cells[a].textContent  === current && cells[b].textContent  === current && cells[c].textContent  === current }
 for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener("click", () => {
         //クリックした回数を数える
@@ -25,11 +25,23 @@ for (let i = 0; i < cells.length; i++) {
     
        if (judge(0,1,2)){
         console.log(current + '勝利');
-    }else if(jusge(3,4,5)){
+    }else if(judge(3,4,5)){
+        console.log(current + '勝利');
+    }else if(judge(6,7,8)){
+        console.log(current + '勝利');
+    }else if(judge(0,3,6)){
+        console.log(current + '勝利');
+    }else if(judge(1,4,7)){
+        console.log(current + '勝利');
+    }else if(judge(2,5,8)){
+        console.log(current + '勝利');
+    }else if(judge(0,4,8)){
+        console.log(current + '勝利');
+    }else if(judge(2,4,6)){
         console.log(current + '勝利');
     }
-        if (cells[0].textContent  === current && cells[4].textContent  === current && cells[8].textContent  === current ){
-            console.log(current + '勝利');
-    }
+    //     if (cells[0].textContent  === current && cells[4].textContent  === current && cells[8].textContent  === current ){
+    //         console.log(current + '勝利');
+    // }
     });
 }
